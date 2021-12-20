f8814a41c95aec412a5aca6211dcd79ecd40a3e5
Maximilian Anderlik <anderlik3@gmail.com>
2021-12-20 03:07:47 +0100

    renamed variables and fixed warnings

--- candlecharts.py
+++ candlecharts.py
@@ -5,25 +5,25 @@ import requests
 import pandas as pd
 
 apikey = os.environ['apikey']
-urlUSD = 'https://financialmodelingprep.com/api/v3/quote/EURUSD?apikey=' + apikey
-urlBTC = 'https://financialmodelingprep.com/api/v3/historical-chart/15min/BTCUSD?apikey=' + apikey
+eur_usd_ratio_request = 'https://financialmodelingprep.com/api/v3/quote/EURUSD?apikey=' + apikey
+btc_request = 'https://financialmodelingprep.com/api/v3/historical-chart/15min/BTCUSD?apikey=' + apikey
 
-eurUSD = requests.get(urlUSD)
-eurUSDResponse = eurUSD.json()
+euro_usd_ratio_response = requests.get(eur_usd_ratio_request)
+eur_usd_ratio = euro_usd_ratio_response.json()
 
-btc = requests.get(urlBTC)
-df = pd.DataFrame.from_dict(btc.json())
+btc = requests.get(btc_request)
+btc_df = pd.DataFrame.from_dict(btc.json())
 
-dfs = df.iloc[0:10]
-dfs['date'] = pd.to_datetime(dfs['date'])
-dfs.date = dfs.date.dt.tz_localize('EST').dt.tz_convert('Europe/Berlin')
-dfs.sort_values(by=['date'], inplace=True, ascending=True)
+btc_df = btc_df.iloc[0:10]
+btc_df['date'] = pd.to_datetime(btc_df['date'])
+btc_df.date = btc_df.date.dt.tz_localize('EST').dt.tz_convert('Europe/Berlin')
+btc_df.sort_values(by=['date'], inplace=True, ascending=True)
 
-fig = go.Figure(data=[go.Candlestick(x=dfs['date'].apply(lambda t: t.strftime('%H:%M')),
-                                     open=dfs['open'] / eurUSDResponse[0]['price'],
-                                     high=dfs['high'] / eurUSDResponse[0]['price'],
-                                     low=dfs['low'] / eurUSDResponse[0]['price'],
-                                     close=dfs['close'] / eurUSDResponse[0]['price']
+fig = go.Figure(data=[go.Candlestick(x=btc_df['date'].apply(lambda t: t.strftime('%H:%M')),
+                                     open=btc_df['open'] / eur_usd_ratio[0]['price'],
+                                     high=btc_df['high'] / eur_usd_ratio[0]['price'],
+                                     low=btc_df['low'] / eur_usd_ratio[0]['price'],
+                                     close=btc_df['close'] / eur_usd_ratio[0]['price']
                                      )])
 
 fig.update_layout(margin=dict(l=0, r=0, t=0, b=0), xaxis_rangeslider_visible=False, height=300, width=300)
